38177c6f128ff8807dc4fa2d71a69b8bef9976b0
Samuel Soriano <chuwi@adsf.es>
2026-05-08 16:00:32 +0200

    idiomas

--- appengine/common/boot.js
+++ appengine/common/boot.js
@@ -23,12 +23,7 @@ if (location.host === 'blockly-games.appspot.com') {
 
   // Supported languages (consistent across all apps).
   window['BlocklyGamesLanguages'] = [
-    'am', 'ar', 'be', 'be-tarask', 'bg', 'bn', 'br', 'ca', 'cs', 'da', 'de',
-    'el', 'en', 'eo', 'es', 'eu', 'fa', 'fi', 'fo', 'fr', 'gl', 'ha', 'he',
-    'hi', 'hr', 'hu', 'hy', 'ia', 'id', 'ig', 'is', 'it', 'ja', 'kab', 'kn',
-    'ko', 'lt', 'lv', 'ms', 'my', 'nb', 'nl', 'pl', 'pms', 'pt', 'pt-br',
-    'ro', 'ru', 'sc', 'sk', 'sl', 'sq', 'sr', 'sr-latn', 'sv', 'th', 'ti',
-    'tr', 'uk', 'ur', 'vi', 'yo', 'zh-hans', 'zh-hant'
+    'ca', 'en', 'es'
   ];
 
   // Use a series of heuristics that determine the likely language of this user.
